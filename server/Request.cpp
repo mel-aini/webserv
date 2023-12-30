d3576a86cdd8bdfcbebbacdd08f82a68083152c7
Houssam Eddine El Mamouni <hel-mamo@e3r6p1.1337.ma>
2023-12-30 11:34:38 +0100

    request parsing works fine need to be tested

--- server/Request.cpp
+++ server/Request.cpp
@@ -1,47 +1,415 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Request.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hel-mamo <hel-mamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 11:54:49 by hel-mamo          #+#    #+#             */
+/*   Updated: 2023/12/24 18:03:49 by hel-mamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Request.hpp"
+#include <unistd.h>
 
-void Request::appendToBuffer(const char *s) {
-    this->buffer += s;
+Request::Request() : status(200), _state(START), _chunkState(CHUNK_SIZE_START), _lengthState(0) , _filename("./server/_files/" + std::to_string(time(0)) + ".tmp")
+{
 }
 
-int Request::getStatus() const {
-    return this->status;
+
+Request::Request(Request const &src)
+{
+    *this = src;
 }
 
-void Request::setStatus(int status) {
-    this->status = status;
-}
-
-bool Request::nextIsBody() {
-    if (this->buffer.length() > 3) {
-        if (this->buffer.compare(this->buffer.length() - 4, 4, "\n\r\n\r") == 0)
-            return true;
-        else if (this->buffer.compare(this->buffer.length() - 2, 2, "\n\n") == 0)
-            return true;
+Request &Request::operator=(Request const &rhs)
+{
+    if (this != &rhs)
+    {
+        this->_state = rhs._state;
+        this->_chunkState = rhs._chunkState;
+        this->_lengthState = rhs._lengthState;
+        this->_request = rhs._request;
+        this->_method = rhs._method;
+        this->_uri = rhs._uri;
+        this->_version = rhs._version;
+        this->_headers = rhs._headers;
+        this->_filename = rhs._filename;
     }
+    return *this;
+}
+
+std::string Request::getMethod()
+{
+    return this->_method;
+}
+
+std::string Request::getUri()
+{
+    return this->_uri;
+}
+
+std::string Request::getVersion()
+{
+    return this->_version;
+}
+
+std::string Request::getHeader(std::string key)
+{
+    std::map<std::string, std::string>::iterator it = this->_headers.find(key);
+    if (it == this->_headers.end())
+        return "";
+    return it->second;
+}
+
+
+bool Request::ContentLengthExists()
+{
+    std::map<std::string, std::string>::iterator it = this->_headers.find("content-length");
+    if (it == this->_headers.end())
+        return false;
+    return true;
+}
+
+bool Request::TransferEncodingExists()
+{
+    std::map<std::string, std::string>::iterator it = this->_headers.find("transfer-encoding");
+    if (it == this->_headers.end())
+        return false;
+    return true;
+}
+
+bool Request::isChunked()
+{
+    if (this->TransferEncodingExists() && this->getTransferEncoding() == "chunked")
+        return true;
     return false;
 }
 
-void Request::resetBuffer() {
-    this->buffer = "";
-}
-
-std::string& Request::getBuffer() {
-    return this->buffer;
-}
-
-int Request::getSize() const {
-    return this->buffer.size();
-}
-
-void    Request::setUri(std::string& uri)
+size_t Request::getContentLenght()
 {
-    this->uri = uri;
+    std::map<std::string, std::string>::iterator it = this->_headers.find("content-length");
+    if (it == this->_headers.end())
+        return 0;
+    return std::stoi(it->second);
 }
 
-const std::string&  Request::getUri() const
+std::string Request::getTransferEncoding()
 {
-    return (this->uri);
+    std::map<std::string, std::string>::iterator it = this->_headers.find("transfer-encoding");
+    if (it == this->_headers.end())
+        return "";
+    return it->second;
 }
 
-Request::~Request() {}
+int Request::thereIsBoundary()
+{
+    std::map<std::string, std::string>::iterator it = this->_headers.find("content-type");
+    if (it == this->_headers.end())
+        return 0;
+    std::string contentType = it->second;
+    if (contentType.find("multipart/form-data") == std::string::npos)
+        return 0;
+    std::stringstream ss(contentType);
+    std::string token;
+    while (std::getline(ss, token, ';'))
+    {
+        if (token.find("boundary") != std::string::npos)
+        {
+            this->_boundary = token.substr(token.find("=") + 1);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int Request::validateRequestLine()
+{
+    std::string allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~:/?#[]@!$&'()*+,;=";
+    if (this->_method != "GET" && this->_method != "POST" && this->_method != "DELETE" 
+        && this->_method != "PUT" && this->_method != "HEAD" && this->_method != "OPTIONS" 
+        && this->_method != "TRACE" && this->_method != "CONNECT")
+    {
+        this->status = 400;
+        return 0;
+    }
+    if (this->_uri.find_first_not_of(allowed) != std::string::npos)
+    {
+        this->status = 400;
+        return 0;
+    }
+    if (this->_uri[0] != '/' || this->_version != "HTTP/1.1")
+    {
+        this->status = 400;
+        return 0;
+    }
+    if (this->_uri.length() > 2048)
+    {
+        this->status = 414;
+        return 0;
+    }
+    return 1;
+}
+
+// int Request::validateHeaderLine()
+// {
+//     std::string allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~:/?#[]@!$&'()*+,;=";
+//     if (this->currentHeaderKey.find_first_not_of(allowed) != std::string::npos)
+//     {
+//         this->status = 400;
+//         return 0;
+//     }
+//     if (this->currentHeaderValue.find_first_not_of(allowed) != std::string::npos)
+//     {
+//         this->status = 400;
+//         return 0;
+//     }
+//     return 1;
+// }
+
+int Request::readHeaders()
+{
+    if (this->_request.find("\r\n\r\n") == std::string::npos)
+        return 1;
+    std::string headers = this->_request.substr(0, this->_request.find("\r\n\r\n"));
+    std::stringstream ss(headers);
+    std::string line;
+    while (std::getline(ss, line, '\n'))
+    {
+        if (line == "\r")
+            break;
+        std::string key;
+        std::string value;
+        std::stringstream ss2(line);
+        std::getline(ss2, key, ':');
+        if (ss2.peek() == ' ')// skip the space after the :
+            ss2.seekg(1, ss2.cur);
+        std::getline(ss2, value, '\r');
+        std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+        this->currentHeaderKey = key;
+        this->currentHeaderValue = value;
+        this->_headers[key] = value;
+    }
+    if (this->TransferEncodingExists() && this->getTransferEncoding() != "chunked")
+    {
+        this->status = 501;
+        return 0;
+    }
+    this->_request = this->_request.substr(this->_request.find("\r\n\r\n") + 4);
+    if (this->isChunked())
+    {
+        this->_state = CHUNKED;
+        this->_lengthState = 0;
+    }
+    else if (this->thereIsBoundary())
+        this->_state = BOUNDARY;
+    else if (this->ContentLengthExists())
+    {
+        this->_state = CONTENT_LENGTH;
+        this->_lengthState = this->getContentLenght();  
+    }
+    else
+    {
+        this->status = 400;
+        return 0;
+    }
+    return 1;
+}
+
+int Request::readByChunk()
+{
+    /*
+       Read the request by chunk
+    */
+   switch (this->_chunkState)
+   {
+        case CHUNK_SIZE_START :
+        {
+            if (this->_request.find("\r\n") == std::string::npos)
+                break;
+            this->_chunkState = CHUNK_SIZE;
+        }
+        case CHUNK_SIZE :
+        {
+            if (this->_lengthState == 0)
+            {
+                std::string chunkSize = this->_request.substr(0, this->_request.find("\r\n"));
+                std::stringstream ss(chunkSize);
+                if(!(ss >> std::hex >> this->_lengthState))
+                {
+                    this->status = 400;
+                    return 0;
+                }
+                if (this->_lengthState == 0)
+                {
+                    this->_state = END; 
+                    return 0;
+                }
+                this->_request = this->_request.substr(this->_request.find("\r\n") + 2);
+                this->_chunkState = CHUNK_DATA;
+            }
+            else
+            {
+                this->_chunkState = CHUNK_SIZE_START;
+                return 1;
+            }
+        }
+        case CHUNK_DATA :
+        {
+            if (this->_request.find("\r\n") == std::string::npos)
+                return 1;
+            std::ofstream file(this->_filename, std::ios::out | std::ios::app);
+            if (this->_lengthState < this->_request.length())
+            {
+                file << this->_request.substr(0, this->_lengthState);
+                this->_request = this->_request.substr(this->_lengthState + 2);
+                this->_lengthState = 0;
+                this->_chunkState = CHUNK_SIZE_START;
+                file.close();
+                return (readByChunk());
+            }
+            else if (this->_lengthState > 0)
+            {
+                if (this->_lengthState > this->_request.length())
+                {
+                    file << this->_request;
+                    this->_lengthState -= this->_request.length();
+                    this->_request = "";
+                    file.close();
+                    return 1;
+                }
+                else if (this->_lengthState == this->_request.length())
+                {
+                    file << this->_request;
+                    this->_lengthState = 0;
+                    this->_request = "";
+                    file.close();
+                    this->_chunkState = CHUNK_SIZE_START;
+                    return 1;
+                }
+            }
+        }
+   }
+    return 1;
+}
+
+int Request::readByContentLength()
+{
+    std::ofstream file(this->_filename, std::ios::out | std::ios::app);
+    if (this->_request.length() > this->_lengthState)
+    {
+        file << this->_request.substr(0, this->_lengthState);
+        this->_request = this->_request.substr(this->_lengthState);
+        file.close();
+        return 1;
+    }
+    this->_lengthState -= this->_request.length();
+    if (this->_lengthState > 0)
+    {
+        file << this->_request;
+        this->_request = "";
+        file.close();
+        return 1;
+    }
+    else if (this->_lengthState == 0)
+    {
+        file << this->_request;
+        this->_request = "";
+        file.close();
+    }
+    this->_state = END;
+    return 0;
+}
+
+int Request::readBoundary()
+{
+    /*
+        append the request to a file until the end of the request
+    */
+    if (this->_request.find(this->_boundary + "--") == std::string::npos)
+    {
+        std::ofstream file(this->_filename, std::ios::out | std::ios::app);
+        file << this->_request;
+        this->_request = "";
+        file.close();
+        return 1;
+    }
+    else 
+    {
+        std::ofstream file(this->_filename, std::ios::out | std::ios::app);
+        file << this->_boundary + "--";
+        this->_request = "";
+        file.close();
+        this->_state = END;
+        return 0;
+    }
+}
+
+int Request::parseRequest(char *buffer, int size, int fd)
+{   
+    this->_request += std::string(buffer, size);
+    this->_fd = std::to_string(fd);
+    switch (this->_state)
+    {
+        case START : 
+        {
+            if (this->_request.find("\r\n") == std::string::npos)
+                break;
+            this->_state = METHOD;
+        }
+        case METHOD:
+        {
+            std::string requestLine = this->_request.substr(0, this->_request.find("\r\n"));
+            std::stringstream ss(requestLine);
+            std::getline(ss, this->_method, ' ');
+            std::getline(ss, this->_uri, ' ');
+            std::getline(ss, this->_version, ' ');
+            if (!validateRequestLine())
+                goto end;
+            this->_request = this->_request.substr(this->_request.find("\r\n") + 2);
+            this->_state = HEADER;
+        }
+        case HEADER :
+        {
+            if (readHeaders())
+                break ;
+            goto end;
+        }
+        case CONTENT_LENGTH :
+        {            
+            if (readByContentLength())
+                break ;
+            goto end;
+        }
+        case CHUNKED :
+        {
+            if (readByChunk())
+                break ;
+            goto end;     
+        }
+        case BOUNDARY :
+        {
+            if (readBoundary())
+               break ;
+        }
+        case END :
+        {
+            end:
+                return 1;
+        }
+    }
+    return 0;
+}
+
+void Request::printRequest()
+{
+    std::cout << "Method: " << this->_method << std::endl;
+    std::cout << "Path: " << this->_uri << std::endl;
+    std::cout << "Version: " << this->_version << std::endl;
+    std::cout << "Headers: " << std::endl;
+    for (std::map<std::string, std::string>::iterator it = this->_headers.begin(); it != this->_headers.end(); it++)
+    {
+        std::cout << it->first << ": " << it->second << std::endl;
+    }
+}
--- server/Request.hpp
+++ server/Request.hpp
@@ -1,44 +1,88 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Request.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hel-mamo <hel-mamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 11:50:56 by hel-mamo          #+#    #+#             */
+/*   Updated: 2023/12/24 16:07:57 by hel-mamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <map>
-#include <iostream>
-#include "Location.hpp"
+# ifndef Request_HPP
+# define Request_HPP
 
-class Request {
+# include <iostream>
+# include <string>
+# include <map>
+# include <vector>
+# include <algorithm>
+# include <iterator>
+# include <sstream>
+# include <fstream>
+
+enum State{
+    START,
+    METHOD,
+    HEADER,
+    CONTENT_LENGTH,
+    CHUNKED,
+    BOUNDARY,
+    END
+};
+
+enum Chunk_State{
+    CHUNK_SIZE_START,
+    CHUNK_SIZE,
+    CHUNK_DATA,
+};
+
+
+class Request
+{
     private:
-        int                                 method;
-        std::string                         uri;
-        std::map<std::string, std::string>  headers;
-        std::string                         buffer;
-        int             					status;
-
-        /*
-            title: hedears we will need
-            1- Host
-            2- Connection
-            3- Content-Length
-            4- Content-Type
-            5- Transfer-Encoding
-        */
-        std::string                         body;
+        int             status;
+        std::string     _fd;
+        State           _state;
+        Chunk_State     _chunkState;
+        size_t          _lengthState;
+        std::string     _request;
+        std::string     _method;
+        std::string     _uri;
+        std::string     _version;
+        std::string     currentHeaderKey;
+        std::string     currentHeaderValue;
+        std::map        <std::string, std::string> _headers;
+        std::string     _filename;
+        std::string     _boundary;
 
     public:
-        Request() : status(0) {};
-        ~Request();
+        Request();
+        Request(Request const &src);
+        Request     &operator=(Request const &rhs);
+        int         parseRequest(char *buffer, int size, int fd);
+        void        printRequest();
+        size_t      getContentLenght();
+        std::string getTransferEncoding();
+        bool        ContentLengthExists();
+        bool        TransferEncodingExists();
+        bool        isChunked();
+        int         readByChunk();
+        int         readByContentLength();
+        int         readHeaders();
+        int         readBoundary();
+        int         validateRequestLine();
+        int         validateHeaderLine();
+        int         thereIsBoundary();
 
-        const std::string&  getUri() const;
-        void                setUri(std::string& uri);
-        int                 getMethod() const;
-        void                appendToBuffer(const char *s);
-        int                 getStatus() const;
-        void                setStatus(int status);
-        bool                nextIsBody();
-        void                resetBuffer();
-        int                 getSize() const;
-        std::string&        getBuffer();
-
-        class RequestFailed : public std::exception {
-			public:
-				const char * what() const throw();
-		};
+        std::string getMethod();
+        std::string getUri();
+        std::string getVersion();
+        std::string getHeader(std::string key);
+        std::string getFilename();
+        std::string getBoundary();
 };
+
+
+#endif
