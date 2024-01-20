2805320aa81243c05a796ef537897a361e81f9a7
Houssam Eddine El Mamouni <hel-mamo@e2r7p14.1337.ma>
2024-01-20 16:23:39 +0100

    save

--- server/Request.cpp
+++ server/Request.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Request.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-aini <mel-aini@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hel-mamo <hel-mamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 11:54:49 by hel-mamo          #+#    #+#             */
-/*   Updated: 2024/01/20 13:08:02 by mel-aini         ###   ########.fr       */
+/*   Updated: 2024/01/20 16:22:43 by hel-mamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -295,7 +295,6 @@ int Request::readHeaders()
         if (ss2.peek() == ' ')// skip the space after the :
             ss2.seekg(1, ss2.cur);
         std::getline(ss2, value, '\r');
-        std::cout << BLUE << value << RESET << std::endl;
         std::transform(key.begin(), key.end(), key.begin(), ::tolower);
         this->currentHeaderKey = key;
         this->currentHeaderValue = trimSpacesAndTabs(value);
@@ -308,7 +307,6 @@ int Request::readHeaders()
     }
     if (!this->getReadingMethod())
         return 0;
-    printRequest();
     return 1;
 }
 
