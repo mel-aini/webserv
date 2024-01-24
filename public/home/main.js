3551c61df756315cdac0289a86722ac2be6447bc
Mohssine El Aini <mel-aini@e2r2p1.1337.ma>
2024-01-24 11:51:08 +0100

    save

--- public/home/main.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll(".expand");
-
-buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		const scope = button.closest(".scope");
-
-		scope.classList.toggle('active');
-	})
-})
