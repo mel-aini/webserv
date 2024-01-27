fcfb8bf3fc51d221876fd76278846f033dd09821
Othman Chouikhane <ochouikh@e2r1p4.1337.ma>
2024-01-27 15:36:11 +0100

    save

--- server/Client.hpp
+++ server/Client.hpp
@@ -61,7 +61,6 @@ class Client
 		void				setPollfd(struct pollfd	*pollfd);
 		void				setServerInfo(std::string port, std::string host, std::string s_name);
 		void				setFirstCgiEnv(void);
-		void				setCookies(void);
 		void				setNeedTransfer(bool state);
 	
         bool				findLocation(std::vector<Location> &locations, std::string uri);
